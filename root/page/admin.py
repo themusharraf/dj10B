dbfd2480ca693a5b847eb507d47cd9fe9d792b0f
mrx <meibrohimov@gmail.com>
2024-12-18 09:33:59 +0500

    Initial commit

---
+++ root/page/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from page.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+    # select * from posts
+
+    # post = Objects.Post.all()
---
+++ root/page/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from page.views import post_detail,post_list
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('',post_list,name="post_list"),
+    path("<int:year>/<int:month>/<int:day>/<slug:post>/",post_detail,name="post_detail")
+]
